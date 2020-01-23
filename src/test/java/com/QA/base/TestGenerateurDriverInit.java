0810d9d42e579e980e03c1585de312128404031f
wboufade <wael.boufaden@advyteam.com>
2020-01-23 13:50:54 +0100

    Adding tags into the test runner

--- src/test/java/com/QA/base/TestGenerateurDriverInit.java
+++ src/test/java/com/QA/base/TestGenerateurDriverInit.java
@@ -25,6 +25,7 @@ import java.util.stream.Collectors;
 @CucumberOptions(
         features = {"src/test/resources/features/"},
         plugin = {"json:target/reports/cucumber.json"},
+        tags = {"@ACCRETIO2-CORERH or @ACCRETIO2-CONNECT"},
         monochrome = true,
         glue = {"com.QA.steps"}
 )
